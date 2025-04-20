4f79d585f29db8b0f911c68cc4811738cbdd35a8
Elijah Abebe <ellayab804@gmail.com>
2025-04-20 10:31:35 +0000

    user route for adding user and geting users

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const connectMongoDB = require('./config/db.js')
+const userRouter = require('./routes/users.js')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.use(express.static('public'))
 
 // Connect DB
 connectMongoDB()
-const userRouter = require('./routes/users.js')
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.use('/api/user', userRouter)
+app.use('/api/users', userRouter)
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,39 @@ const User = require('../models/User')
 const Exercise = require('../models/Exercise')
 
 router.post('/', async (req, res) => {
-    const userName = req.body.userName
-    if(!userName) res.status(400).json({error: 'User name is required'})
-
-        
+    const username = req.body.username
+    if(!username) {
+        return res.status(400).json({error: 'User name is required'})
+}
+    const user = await User.findOne({username})
+    if(user) {
+        return res.json({username: user.username, _id: user._id})
+    }
     try {
-        const newUser = newUser
+        const newUser = new User({username})
+        const savedUser = await newUser.save()
+        res.json({username: savedUser.username, _id: savedUser._id})
     } catch (error) {
-        
+        res.status(500).json({error: error.message})
+    }
+})
+
+router.get('/:id/logs', async(req, res) => {
+    const id = req.params.id
+    try {
+        const user = await User.findById(id)
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
+router.get('/', async (_, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({error: error.message})
     }
 })
 
