8b5120e5420005686a2cf37f4285db92314a4594
Elijah Abebe <ellayab804@gmail.com>
2025-04-20 20:12:16 +0000

    all route implemented

--- routes/users.js
+++ routes/users.js
@@ -21,13 +21,40 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.post("/:id/exercises", async (req, res) => {
+    const userId = req.params.id
+    const { description, duration, date } = req.body;
+
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: 'User not found' });
+  
+    const exercise = new Exercise({
+      userId: user._id,
+      description,
+      duration: parseInt(duration),
+      date: date ? new Date(date) : new Date()
+    });
+  
+    const savedExercise = await exercise.save();
+  
+    res.json({
+      _id: user._id,
+      username: user.username,
+      date: savedExercise.date.toDateString(),
+      duration: savedExercise.duration,
+      description: savedExercise.description
+    });
+})
+
 router.get('/:id/logs', async(req, res) => {
     const id = req.params.id
+
     try {
         const user = await User.findById(id)
-        res.status(200).json(user)
+        const exercise = await Exercise.find({userId: user._id})
+        res.status(200).json({username: user.username, _id: user._id, count: exercise.length, log: exercise})
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({error: "User not found"})
     }
 })
 
